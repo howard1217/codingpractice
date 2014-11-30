7fcc02d5d2474e3382908ea9430726534b527aad
Howard <howardchiao1217@gmail.com>
2014-11-30 17:32:46 +0100

    searchMatrix

--- Leetcode3.java
+++ Leetcode3.java
@@ -663,4 +663,32 @@ public class Leetcode3 extends Leetcode {
         }
         return result;
     }
+
+    /** Write an efficient algorithm that searches for a value in an m x n matrix.
+     *  This matrix has the following properties:
+        Integers in each row are sorted from left to right.
+        The first integer of each row is greater than the last integer of the previous row.
+        For example,
+        Consider the following matrix:
+        [
+          [1,   3,  5,  7],
+          [10, 11, 16, 20],
+          [23, 30, 34, 50]
+        ]
+        Given target = 3, return true.
+     *  https://oj.leetcode.com/problems/search-a-2d-matrix/ */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) return false;
+        if (matrix == null || matrix.length == 0) return false;
+        int m = matrix.length, n = matrix[0].length;
+        int a = 0, b = m * n - 1, curr = (a + b) / 2;
+        while (a < b - 1) {
+            int i = curr / n, j = curr % n;
+            if (matrix[i][j] > target) b = curr;
+            else if (matrix[i][j] < target) a = curr;
+            else return true;
+            curr = (a + b) / 2;
+        }
+        return (matrix[a / n][a % n] == target) || (matrix[b / n][b % n] == target);
+    }
 }
